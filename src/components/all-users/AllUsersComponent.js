4b823020d0a47e6fe4a07ae4da9da924bc45b42d
light86neon <72137342+light86neon@users.noreply.github.com>
2020-12-05 22:49:06 +0300

    додали кнопку щоб виводити користувачів

--- src/components/all-users/AllUsersComponent.js
+++ src/components/all-users/AllUsersComponent.js
@@ -5,7 +5,7 @@ import './AllUsers.css'
 class AllUsersComponent extends Component {
     // mistake
     // users = [];
-    state = {users: [], classState :'one'};
+    state = {users: [], classState :'one', chosenUser: null};
 //якщо не буде пустого масиву не зможемо використати методи масив(map)
     // flag = false
     // classState = 'one';
@@ -26,7 +26,6 @@ flag = false;
         // }
         // this.flag = !this.flag
         // console.log(this.classState, this.flag)
-
         if(this.flag){
             this.setState({classState: 'one'});
         } else {
@@ -34,15 +33,28 @@ flag = false;
         }
         this.flag = !this.flag;
     };
+
+    selectThisUser = (id) => {
+       let chosenUser = this.state.users.find(value => value.id === id);
+    this.setState({chosenUser})
+    };
+
+
     render() {
-        let {users,classState} = this.state;
+        let {users,classState,chosenUser} = this.state;
 // console.log(this.state)
         return (
             <div>
                 <h1 onClick={this.changeColor} className={classState}>All Users Page</h1>
                 {
             users.map(value => (<UserComponent
-                item={value} key={value.id}/>))
+                item={value} key={value.id}
+                selectThisUser = {this.selectThisUser}
+            />))
+                }
+                <hr/>
+                {
+                    chosenUser && <UserComponent item ={chosenUser}/>
                 }
             </div>
         );
--- src/components/user/UserComponent.js
+++ src/components/user/UserComponent.js
@@ -3,10 +3,11 @@ import React, {Component} from 'react';
 class UserComponent extends Component {
     render() {
 
-        let {item} = this.props;
+        let {item,selectThisUser} = this.props;
         return (
             <div>
                 {item.id} - {item.name}
+                <button onClick={() => selectThisUser(item.id)}>choose my</button>
             </div>
         );
     }
