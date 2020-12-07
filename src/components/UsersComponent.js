9311fc084c54319d8ac834fafe40f53709be8af5
light86neon <72137342+light86neon@users.noreply.github.com>
2020-12-07 23:06:04 +0200

    home work1

---
+++ src/components/UsersComponent.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class UsersComponent extends Component {
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>Hello Users Component</h1>
+            </div>
+        );
+    }
+}
+
+export default UsersComponent;
